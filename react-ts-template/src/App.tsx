febfb55cd31de70f9ca63a245f55e737511f82b3
yasuda19192 <yasuda19192@gmail.com>
2024-12-05 02:31:00 +0900

    tss input form &useState
    motto kisowo yatte kara sawaru

--- react-ts-template/src/App.tsx
+++ react-ts-template/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Todo = {
+  value: string;
+};
+
+export const App = () => {
+  const [text, setText] = useState("");
+  console.log("renderd(App.tsx)");
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type="submit"
+            value="追加"
+            onSubmit={(e) => e.preventDefault()}
+          />
+        </form>
+        <p>{text}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
